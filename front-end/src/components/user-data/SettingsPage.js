62834312b37024a10894c86ca6bcb67519b237db
sanay-d-nyu <sd5113@nyu.edu>
2025-12-09 18:55:05 -0500

    Test cd workflow again

--- front-end/src/components/user-data/SettingsPage.js
+++ front-end/src/components/user-data/SettingsPage.js
@@ -118,7 +118,7 @@ export default function SettingsPage() {
             <label>New Password:</label>
             <input 
               type="password"
-              placeholder="New passwordssssss"
+              placeholder="New passwordss"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
